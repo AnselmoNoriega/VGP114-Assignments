843b1ed698f77b7e443d14b4630f4c291b1bf7ac
AnselmoNoriega <122755063+AnselmoNoriega@users.noreply.github.com>
2023-01-19 18:56:02 -0500

    Add files via upload

---
+++ TikTak/TikTak/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace TicTacToe;
+using static System.Console;
+
+public class Program
+{
+    public static void Main()
+    {
+        var ticTacToe = new TicTacToe();
+        int rowNum, columnNum;
+        int xWinCount = 0, oWinCount = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            ticTacToe.NewBoard();
+            WriteLine(ticTacToe.ToString());
+
+            while (!ticTacToe.IsGameOver())
+            {
+                ticTacToe.WrittenGetTurn();
+                WriteLine("\nIn which row do you want to draw your turn?");
+                string rowTurn = ReadLine();
+
+                WriteLine("In which column do you want to draw your turn?");
+                string columnTurn = ReadLine();
+                WriteLine("\n\n");
+
+
+                int.TryParse(rowTurn, out rowNum);
+                int.TryParse(columnTurn, out columnNum);
+
+                ticTacToe.Move(rowNum, columnNum);
+                WriteLine(ticTacToe.ToString());
+            }
+
+            if(ticTacToe.GetWinner() == Player.X)
+            {
+                xWinCount++;
+                WriteLine("\n\nX Wins = {0}", xWinCount, "\n\n");
+                if(xWinCount== 3) { i = 5; };
+            }
+            else if (ticTacToe.GetWinner() == Player.O)
+            {
+                oWinCount++;
+                WriteLine("\n\nO Wins = {0}", oWinCount, "\n\n");
+                if (oWinCount == 3) { i = 5; };
+            }
+            else
+            {
+                WriteLine("\n\nStalemate\n\n");
+            }
+        }
+    }
+}
---
+++ TikTak/TikTak/TicTacToe.cs
@@ -0,0 +1,140 @@
+﻿
+
+namespace TicTacToe;
+
+public class TicTacToe : ITicTacToe
+{
+    Player[][] map = new Player[][]
+        {
+        new Player[] { Player.None, Player.None, Player.None},
+        new Player[] { Player.None, Player.None, Player.None},
+        new Player[] { Player.None, Player.None, Player.None}
+        };
+
+
+    public void NewBoard()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                map[i][j] = Player.None;
+            }
+        }
+    }
+
+    int turnIndex = 1;
+
+    public void Move(int r, int c)
+    {
+        if ((r < 3 && r >= 0) && (c < 3 && c >= 0))
+        {
+            if (map[r][c] == Player.None)
+            {
+                map[r][c] = GetTurn();
+                turnIndex++;
+            }
+        }
+        else 
+        {
+            Console.WriteLine("\"[Does coordinates does not exist]\"\n\n");
+        }
+    }
+    public void WrittenGetTurn()
+    {
+        if ((turnIndex % 2) == 0)
+        {
+            Console.WriteLine("[O's turn]");
+        }
+        else
+        {
+            Console.WriteLine("[X's turn]");
+        }
+    }
+    public Player GetTurn()
+    {
+        if ((turnIndex % 2) == 0)
+        {
+            return Player.O;
+        }
+        else
+        {
+            return Player.X;
+        }
+    }
+
+    public bool IsGameOver()
+    {
+        if (turnIndex == 9)
+        {
+            return true;
+        }
+        else if (((map[0][0] == map[1][1] && map[1][1] == map[2][2]) || (map[2][0] == map[1][1] && map[1][1] == map[0][2])) && map[1][1] != Player.None)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if ((map[0][i] == map[1][i] && map[1][i] == map[2][i] && map[0][i] != Player.None) || (map[i][0] == map[i][1] && map[i][1] == map[i][2] && map[i][0] != Player.None))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Player GetWinner()
+    {
+        turnIndex= 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (map[0][i] == map[1][i] && map[1][i] == map[2][i])
+            {
+                return map[0][i];
+            }
+            else if(map[i][0] == map[i][1] && map[i][1] == map[i][2])
+            {
+                return map[i][0];
+            }
+        }
+
+        if ((map[0][0] == map[1][1] && map[1][1] == map[2][2]) || (map[2][0] == map[1][1] && map[1][1] == map[0][2]))
+        {
+            return map[1][1];
+        }
+
+        return Player.None;
+    }
+
+    public Player[][] GetBoard()
+    {
+        return map;
+    }
+
+    public Player GetMarkAt(int r, int c)
+    {
+        return map[r][c];
+    }
+
+    // add your implementation here
+    public override string ToString()
+    {
+        var rows = new List<string>();
+        foreach (var row in GetBoard())
+        {
+            var rowStrings = new List<string>();
+            foreach (var p in row)
+                if (p == Player.None)
+                    rowStrings.Add(" ");
+                else
+                    rowStrings.Add(p.ToString());
+            rows.Add(" " + string.Join(" | ", rowStrings));
+        }
+
+        return string.Join("\n-----------\n", rows);
+    }
+}
+
